0a91a5bea1995ddb6a54e41a2e32dd6f5b0011ac
Lauren Baillot <lbaillot@ensc.fr>
2018-03-15 09:07:59 +0100

    Niveaux

--- ProjetProlog/ProjetProlog.pl
+++ ProjetProlog/ProjetProlog.pl
@@ -1,36 +1,49 @@
-% REGLES DU jEU ET OPERATEURS PERMANENTS
-findujeu :- voiture(j,_,_,(_,_,3,6)).
-:- dynamic(voiture/4).  %définition dynamiques des voitures car on va en enlever et en créer
+%-------------------------------------
+%             RUSH HOUR
+%------------------------------------
 
+%---------------------------------------
+% REGLES DU jEU ET OPERATEURS PERMANENTS
+%---------------------------------------
+
+findujeu :- voiture(j,_,_,(_,_,3,6)).          %Lorsque le joueur a sa dernière composante sur la case 3,6 il a gagné
+:- dynamic(voiture/4).                         %définition dynamiques des voitures car on va en enlever et en créer
+
+
+%----------------------
 % DEFINITION DES OBjETS
+%----------------------
+
 %les voitures verticales sont définies de haut en bas et les voitures horizontales de gauche à droite
 %voiture (I,T,O,A) :- identifiant(I),taille(T),orientation(O), adresse(A).
 
-%niveau 1 
-voiture(1,2,horizontal,(1,1,1,2)).
-voiture(2,3,vertical,(2,1,3,1,4,1)).
-voiture(3,2,vertical,(5,1,6,1)).
-voiture(4,3,vertical,(1,6,2,6,3,6)).
-voiture(5,3,vertical,(2,4,3,4,4,4)).
-voiture(6,2,horizontal,(5,5,5,6)).
-voiture(7,3,horizontal,(6,3,6,4,6,5)).
-voiture(j,2,horizontal,(3,2,3,3)).
+niveau1(Commande) :- Commande == 1,
+    voiture(1,2,horizontal,(1,1,1,2)),
+    voiture(2,3,vertical,(2,1,3,1,4,1)),
+    voiture(3,2,vertical,(5,1,6,1)),
+    voiture(4,3,vertical,(1,6,2,6,3,6)),
+    voiture(5,3,vertical,(2,4,3,4,4,4)),
+    voiture(6,2,horizontal,(5,5,5,6)),
+    voiture(7,3,horizontal,(6,3,6,4,6,5)),
+    voiture(j,2,horizontal,(3,2,3,3)).
 
-%niveau 2
-% voiture(1,2,vertical,(1,1,2,1)).
-% voiture(2,3,horizontal,(4,1,4,2,4,3)).
-% voiture(3,2,horizontal,(6,1,6,2)).
-% voiture(4,2,vertical,(5,3,6,3)).
-% voiture(5,2,horizontal,(6,4,6,5)).
-% voiture(6,2,horizontal,(5,5,5,6)).
-% voiture(7,3,vertical,(2,6,3,6,4,6)).
-% voiture(8,2,vertical,(3,5,4,5)).
-% voiture(9,2,vertical,(2,4,3,4)).
-% voiture(10,3,horizontal,(1,4,1,5,1,6)).
-% voiture(j,2,horizontal,(3,1,3,2)).
+niveau2(Commande) :- Commande == 2,
+    voiture(1,2,vertical,(1,1,2,1)),
+    voiture(2,3,horizontal,(4,1,4,2,4,3)),
+    voiture(3,2,horizontal,(6,1,6,2)),
+    voiture(4,2,vertical,(5,3,6,3)),
+    voiture(5,2,horizontal,(6,4,6,5)),
+    voiture(6,2,horizontal,(5,5,5,6)),
+    voiture(7,3,vertical,(2,6,3,6,4,6)),
+    voiture(8,2,vertical,(3,5,4,5)),
+    voiture(9,2,vertical,(2,4,3,4)),
+    voiture(10,3,horizontal,(1,4,1,5,1,6)),
+    voiture(j,2,horizontal,(3,1,3,2)).
 
-
+%--------------------------
 %CASE PRISE PAR UNE VOITURE
+%--------------------------
+
 caseprise(X,Y,I) :- 
     voiture(I,_,_,(X,Y,_,_)) ;
     voiture(I,_,_,(_,_,X,Y)).
@@ -39,7 +52,11 @@ caseprise(X,Y,I) :-
     voiture(I,_,_,(_,_,X,Y,_,_)) ;
     voiture(I,_,_,(_,_,_,_,X,Y)).
 
+
+%-------------------
 % BLOCAGE PAR UN MUR
+%-------------------
+
 bloquemurG(V) :- 
     voiture(V,2,horizontal,(_,1,_,_)) ;
     voiture(V,3,horizontal,(_,1,_,_,_,_)).
@@ -53,7 +70,11 @@ bloquemurB(V) :-
     voiture(V,2,vertical,(_,_,6,_)) ; 
     voiture(V,3,vertical,(_,_,_,_,6,_)).
 
+
+%------------------------
 % BLOCAGE PAR UNE VOITURE
+%------------------------
+
 %voiture horizontale bloquee par une voiture
 bloquevoitureG(V) :- 
     voiture(V,2,horizontal,(X1,Y1,_,_)) ,
@@ -87,8 +108,12 @@ bloquevoitureB(V) :-
     voiture(V,3,vertical,(_,_,_,_,X3,Y3)) ,
     X is X3+1 ,
     caseprise(X,Y3,_).
-         
+
+
+%------------------------   
 % MOUVEMENT D'UNE VOITURE
+%------------------------
+
 bouger(V,Dir) :- 
     Dir == droite , voiture(V,2,horizontal,(X1,Y1,X2,Y2)) ,
     \+bloquevoitureD(V) , 
@@ -171,8 +196,10 @@ bouger(V,Dir) :-
     Xn3 is X3+1 , 
     retract(voiture(V,3,vertical,(X1,Y1,X2,Y2,X3,Y3))), 
     assert(voiture(V,3,vertical,(Xn,Y1,Xn2,Y2,Xn3,Y3))).
-
+%---------
 %AFFICHAGE
+%---------
+
 affichageGrille(Ligneactu) :- 
     Ligneactu == 7, 
     nl.
@@ -203,4 +230,9 @@ affichageCellule(Ligneactu,Colonneactu) :-
     write("-"),
     put(32).
 
+%---------------
 %GESTION DU JEU
+%--------------
+
+menu():-
+    write("Choisissez le niveau en entrant niveau(1) pour le niveau 1 et niveau(2) pour le niveau 2.").
